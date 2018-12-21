536849d4d27bdd10720784a7d2f890fe65cd4f06
Travis CI User <travis@example.org>
2018-12-21 06:50:33 +0000

    Documentation for gyng/synthrs@7c233a2

---
+++ source-files.js
@@ -0,0 +1,2 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex['synthrs'] = {"name":"","dirs":[],"files":["errors.rs","filter.rs","lib.rs","midi.rs","music.rs","sample.rs","synthesizer.rs","wave.rs","writer.rs"]};
